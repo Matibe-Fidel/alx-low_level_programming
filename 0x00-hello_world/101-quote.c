60bc275ea472c0e845123b7d0cce1b70955298ea
Matibe-Fidel <djef235@gmail.com>
2022-09-08 17:30:01 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0 if the program was runnig successfuly
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
