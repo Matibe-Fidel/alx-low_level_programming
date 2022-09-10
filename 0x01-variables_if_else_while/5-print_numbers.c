3ca7258433c2a47cae173fc70d07496276a97644
Matibe-Fidel <djef235@gmail.com>
2022-09-09 17:11:58 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - alphabet
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9' ; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
