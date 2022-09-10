7ae7ad032936e8ccdb1fa76548187d5b3e671b4a
Matibe-Fidel <djef235@gmail.com>
2022-09-09 18:11:07 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,15 +15,15 @@ int main(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9' ; a++)
+	for (a = 0; a <= 9 ; a++)
 	{
 		putchar(a);
-	if (a != 9)
-	{
-		putchar(',');
-		putchar(' ');
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
 
-	}
+		}
 	}
 
 	putchar('\n');
