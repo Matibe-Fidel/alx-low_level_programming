b86dc077481594e3ed36dc304bf9d8e307bf8791
Matibe-Fidel <djef235@gmail.com>
2022-09-09 18:06:19 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - alphabet
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9' ; a++)
+	{
+		putchar(a);
+	if (a != 9)
+	{
+		putchar(',');
+		putchar(' ');
+
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
