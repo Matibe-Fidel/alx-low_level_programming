3bae38dd49699510dd4c667922c0b2b048f7b5fa
Matibe-Fidel <djef235@gmail.com>
2022-09-13 16:44:30 -0700

    Putchar function

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+
+
+/**
+ *
+ * *_putchar-writesthecharacterctostdout
+ *
+ * *@c:Thecharactertoprint
+ *
+ * *
+ *
+ * *Return:Onsuccess1.
+ *
+ * *Onerror,-1isreturned,anderrnoissetappropriately.
+ *
+ * */
+
+int _putchar(char c)
+
+{
+
+return (write(1, &c, 1));
+
+}
