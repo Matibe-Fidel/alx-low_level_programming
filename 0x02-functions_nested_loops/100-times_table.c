8cef17af2580e537cc01764b3e68abe0d92c859b
Matibe-Fidel <djef235@gmail.com>
2022-09-14 10:12:54 -0700

    n time tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,43 @@
 #include "main.h"
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * print_times_table - prints times table
- * @n : times table to use
- * Return:void
+ * print_times_table - check the code
+ * @n: parameter
  */
 void print_times_table(int n)
 {
+	int i;
+	int j;
 
-int a = 0, rep, b;
-
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
+	if (n >= 0 && n <= 15)
 	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+		for (i = 0 ; i <= n ; i++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
-		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
+			for (j = 0 ; j <= n ; j++)
+			{
+				if (j == 0)
+				{
+					printf("0");
+				}
+				else
+				{
+					if ((j * i) < 10)
+					{
+						printf(",   %d", j * i);
+					}
+					else if ((j * i) >= 10 && (j * i) < 100)
+					{
+						printf(",  %d", j * i);
+					}
+					else
+					{
+						printf(", %d", j * i);
+					}
+				}
+			}
+			printf("\n");
 		}
 	}
-	_putchar('\n');
-	a++;
-}
 }
