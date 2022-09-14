a1ab1feff5da56eabff9fdddb61987c08179231f
Matibe-Fidel <djef235@gmail.com>
2022-09-13 18:14:17 -0700

    ten times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char a;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (a = 'a' ; a <= 'z' ; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
