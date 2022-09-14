c5fd6adfb6759ec75c0a9aecd3051e2996c54443
Matibe-Fidel <djef235@gmail.com>
2022-09-13 17:05:55 -0700

    Putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
 #include "main.h"
+
+ /*more headers go there */
+/**
+ * main - the main function
+ *
+ * Return: 0 is success
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return(0);
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+	return (0);
 }
