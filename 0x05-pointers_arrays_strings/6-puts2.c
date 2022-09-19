c97663d81efcfbd6df49b31f5e443204e5f4fef2
Matibe-Fidel <djef235@gmail.com>
2022-09-19 11:09:32 -0700

    Every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,26 +3,21 @@
 /**
  * rev_string - reverses a string
  * @s: string to be reversed
+ *@str: string to print the chars from
  */
 void rev_string(char *s)
 {
-	char tmp;
-	int i, len, len1;
+	int len, i;
 
 	len = 0;
-	len1 = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-	len1 = len - 1;
-
 	for (i = 0; i < len / 2; i++)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		_putchar(str[i]);
 	}
 }
